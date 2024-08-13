b8d8cda675a40e388e2c6c4666f84236379e2020
ADEGBITE JOSHUA ADEOYE <adegbitejoshua07@gmail.com>
2024-08-13 22:54:34 +0100

    Learning laravel for making Api

---
+++ database/migrations/2019_08_19_000000_create_failed_jobs_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateFailedJobsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('failed_jobs', function (Blueprint $table) {
+            $table->id()->autoIncrement();
+            $table->string('uuid')->charset('utf8')->unique();
+            $table->text('connection')->charset('utf8');
+            $table->text('queue')->charset('utf8');
+            $table->longText('payload')->charset('utf8');
+            $table->longText('exception')->charset('utf8');
+            $table->timestamp('failed_at')->useCurrent();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('failed_jobs');
+    }
+}
